9f91d2c75636c8110a229f8cda63627f48e6b8c1
farhutdinoff <33476367+farhutdinoff@users.noreply.github.com>
2022-07-11 21:21:16 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,3 +13,38 @@ $(document).ready(function() {
 
 
 });
+
+
+
+
+$(document).ready(function(){
+
+  $('#logo-bg').children('div').each(function(n_div,div)
+  {
+  $(div).children('img').each(function(n_img,img)
+  {
+  if(n_img) $(img).css({'opacity':'0.0'});
+  else
+  {
+  $(div).attr({'value':(n_img+1)});
+  $(img).css({'opacity':'1.0'});
+  }
+  });
+  });
+  window.setInterval(function()
+  {
+  $('#logo-bg').children('div').each(function(n,element)
+  {
+  var img = $(element).attr('value');
+  var rnd = Math.floor(Math.random()*(4-1+1))+1;
+  if(img != rnd)
+  {
+  $(element).attr({'value':rnd});
+  $(element).children('#logo-bg__img'+(n+1)+img).animate(
+  {'opacity':'0.0'});
+  $(element).children('#logo-bg__img'+(n+1)+rnd).animate(
+  {'opacity':'1.0'});
+  }
+  });
+  },2000);
+});
